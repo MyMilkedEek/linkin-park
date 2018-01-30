32b8ffa61dbfa72fa00b82a8e3f01a255929e4af
Michael Demey <mikkeldemey@gmail.com>
2018-01-30 22:21:44 +0100

    recursive fetching of links.

---
+++ src/main/java/net/mymilkedeek/linkinpark/repository/FileSystem.java
@@ -0,0 +1,48 @@
+package net.mymilkedeek.linkinpark.repository;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+
+/**
+ * @author MyMilkedEek <Michael>
+ */
+class FileSystem {
+
+    private File rootFolder;
+
+    private static final String rootFolderName = "/wikigame/";
+
+    FileSystem() {
+        this.rootFolder = new File(System.getProperty("user.home") + rootFolderName);
+
+        if ( !this.rootFolder.exists() ) {
+            this.rootFolder.mkdirs();
+        }
+    }
+
+    /**
+     * Writes an article's links to the file system.
+     *
+     * @param articleName name of the article to be written
+     * @param links contained in the article
+     * @throws IOException
+     */
+    void writeToArticleFile(String articleName, Collection<String> links) throws IOException {
+        File file = new File(this.rootFolder, articleName);
+        if ( file.exists() ) {
+            file.delete();
+        }
+
+        for ( String link : links ) {
+            FileUtils.writeStringToFile(file, link, "UTF-8", true);
+            FileUtils.writeStringToFile(file, "\n", "UTF-8", true);
+        }
+    }
+
+    boolean hasArticleAlreadyBeenParsed(String articleName) {
+        return new File(this.rootFolder, articleName).exists();
+    }
+}
--- src/main/java/net/mymilkedeek/linkinpark/repository/WikipediaRepository.java
+++ src/main/java/net/mymilkedeek/linkinpark/repository/WikipediaRepository.java
@@ -1,59 +1,37 @@
 package net.mymilkedeek.linkinpark.repository;
 
-import org.apache.commons.io.FileUtils;
-
-import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.Collection;
 
 /**
  * @author MyMilkedEek <Michael>
  */
 public class WikipediaRepository {
 
+    private static final int MAX_DEPTH = 1;
+
     private PageParser pageParser;
+    private FileSystem fileSystem;
 
     public WikipediaRepository() {
         this.pageParser = new PageParser();
+        this.fileSystem = new FileSystem();
     }
 
     public void addPageToRepository(String articleName) throws IOException {
-        Set<String> parsedLinks = (Set<String>) this.pageParser.parseToLinks(articleName, false);
-        Set<String> parsedLinksMobile = (Set<String>) this.pageParser.parseToLinks(articleName, true);
-
-        Set<String> sharedLinks = new HashSet<String>();
-        Set<String> onlyDesktop = new HashSet<String>();
-        Set<String> onlyMobile  = new HashSet<String>();
-
-        for ( String link : parsedLinks ) {
-            if ( parsedLinksMobile.contains(link) ) {
-                sharedLinks.add(link);
-            } else {
-                onlyDesktop.add(link);
-            }
-        }
-
-        for ( String link : parsedLinksMobile ){
-            if ( !sharedLinks.contains(link) ) {
-                onlyMobile.add(link);
-            }
-        }
-
-        write(parsedLinks, new File("desktop.txt"));
-        write(parsedLinksMobile, new File("mobile.txt"));
-        write(sharedLinks, new File("shared.txt"));
-        write(onlyDesktop, new File("onlyDesktop.txt"));
-        write(onlyMobile, new File("onlymobile.txt"));
-
-
-        System.out.println();
+        this.addPageToRepository(articleName, 0);
     }
 
-    private void write(Collection<String> content, File file) throws IOException {
-        file.delete();
-        for ( String line : content ) {
-            FileUtils.writeStringToFile(file, line, "UTF-8", true);
-            FileUtils.writeStringToFile(file, "\n", "UTF-8", true);
+    public void addPageToRepository(String articleName, int currentDepth) throws IOException {
+        if ( ! this.fileSystem.hasArticleAlreadyBeenParsed(articleName) ) {
+            Collection<String> parsedLinks = this.pageParser.parseToLinks(articleName, false);
+            this.fileSystem.writeToArticleFile(articleName, parsedLinks);
+
+            if ( currentDepth <= MAX_DEPTH ) {
+                for (String parsedLink : parsedLinks) {
+                    this.addPageToRepository(parsedLink, currentDepth + 1);
+                }
+            }
         }
     }
 }
